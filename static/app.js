9cb3170458fa3a0d0670e4c3bd15ba47b15002d6
Deemah67 <dalhedaithy@gmail.com>
2022-02-08 16:24:45 +0300

    Ss

---
+++ static/app.js
@@ -0,0 +1,97 @@
+class Chatbox {
+   constructor() {
+       this.args = {
+           openButton: document.querySelector('.chatbox__button'),
+           chatBox: document.querySelector('.chatbox__support'),
+           sendButton: document.querySelector('.send__button')
+       }
+
+       this.state = false; // chatbot is closed
+       this.messages = []; // array to store msgs
+   }
+    display() {
+     const {openButton, chatBox, sendButton} = this.args;
+     const key = "";
+     openButton.addEventListener('click',listener => this.toggleState(chatBox))
+     sendButton.addEventListener('click',listener => this.onSendButton(chatBox))
+     const node = chatBox.querySelector('input');
+     node.addEventListener("keypress",({key:string})=> {
+
+       if (key==="Enter") {
+        document.getElementById("send").click();
+         }
+
+
+     })
+
+
+
+    }
+
+   toggleState(chatbox) { // show or hide the box
+     this.state = !this.state;
+
+      if(this.state) {
+
+        chatbox.classList.add('chatbox--active')
+      } else {
+          chatbox.classList.remove('chatbox--active')
+      }
+
+
+   }
+
+   onSendButton(chatbox) {
+       var textField = chatbox.querySelector('input'); // extract user input
+       let text1 = textField.value
+       if (text1 === "") {
+           return;
+       }
+
+       let msg1 = { name:"user", message: text1 }
+       this.messages.push(msg1);
+
+       fetch($SCRIPT_ROOT + '/predict', {
+           method: 'POST',
+           body: JSON.stringify({message: text1}),
+           crossDomain: true,
+           Headers: {
+               'Content-Type': 'application/json; charset=UTF-8'
+           },
+
+   })
+   //extract the response from chat.py
+   .then(r => r.json())
+   .then(r =>{
+    let msg2 = { name:"فريدة", message: r.answer};
+    this.messages.push(msg2)
+    this.updateChatText(chatbox)
+    textField.value = ''
+   }).catch((error) => {
+
+   /* console.error('error:', error); */
+    this.updateChatText(chatbox)
+    textField.value = ''
+   });
+
+   }
+
+   updateChatText(chatbox) {
+       var html = '';
+       this.messages.slice().reverse().forEach(function(item, ) {
+         if (item.name === "فريدة")
+         {
+             html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
+         } else {
+            html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
+         }
+
+
+       });
+       const chatmessage = chatbox.querySelector('.chatbox__messages');
+       chatmessage.innerHTML = html;
+   }
+}
+
+const chatbox = new Chatbox();
+chatbox.display();
